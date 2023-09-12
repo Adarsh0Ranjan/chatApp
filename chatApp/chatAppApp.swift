5c67db9feeb44fdf3ca981c33c84aca5c158ea8e
Adarsh Ranjan Roro <adarsh.ranjan@roro.io>
2023-09-12 15:05:50 +0530

    Initial Commit

---
+++ chatApp/chatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatAppApp.swift
+//  chatApp
+//
+//  Created by Adarsh Ranjan on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct chatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
